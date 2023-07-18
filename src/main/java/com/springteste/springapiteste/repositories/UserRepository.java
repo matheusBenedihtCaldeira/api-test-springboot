778d4f61aa653bdf50272ce04deb26078d037c87
bene <matheusbenediht@gmail.com>
2023-07-18 14:17:04 -0300

    User service, findAll users e findById

--- src/main/java/com/springteste/springapiteste/repositories/UserRepository.java
+++ src/main/java/com/springteste/springapiteste/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.springteste.springapiteste.repositories;
 import com.springteste.springapiteste.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
---
+++ src/main/java/com/springteste/springapiteste/services/UserService.java
@@ -0,0 +1,25 @@
+package com.springteste.springapiteste.services;
+
+import com.springteste.springapiteste.entities.User;
+import com.springteste.springapiteste.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> user = repository.findById(id);
+        return user.get();
+    }
+}
