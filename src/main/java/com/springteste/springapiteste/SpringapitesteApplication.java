9335073291f851381283aa08a74c3d2b661a6b3a
bene <matheusbenediht@gmail.com>
2023-07-18 11:16:06 -0300

    first commit

---
+++ src/main/java/com/springteste/springapiteste/SpringapitesteApplication.java
@@ -0,0 +1,13 @@
+package com.springteste.springapiteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringapitesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringapitesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springteste/springapiteste/SpringapitesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springteste.springapiteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringapitesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
